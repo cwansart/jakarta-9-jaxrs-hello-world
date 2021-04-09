38ba7dd3a06f0f371721db5e93cefcbd82ee7d13
Christian Wansart <christian.wansart@openknowledge.de>
2021-04-09 18:26:09 +0200

    Fix?

--- src/main/java/de/cwansart/helloworld/JaxRsActivator.java
+++ src/main/java/de/cwansart/helloworld/JaxRsActivator.java
@@ -3,6 +3,6 @@ package de.cwansart.helloworld;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("api")
 public class JaxRsActivator extends Application {
 }
