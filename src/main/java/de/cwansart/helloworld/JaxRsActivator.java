386769eaaa7c8575d5af69aecc3fb57413e81288
Christian Wansart <christian.wansart@openknowledge.de>
2021-04-09 18:17:52 +0200

    Initial commit

---
+++ src/main/java/de/cwansart/helloworld/JaxRsActivator.java
@@ -0,0 +1,8 @@
+package de.cwansart.helloworld;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxRsActivator extends Application {
+}
---
+++ src/main/java/de/cwansart/helloworld/Resource.java
@@ -0,0 +1,16 @@
+package de.cwansart.helloworld;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/res")
+@Produces(MediaType.TEXT_PLAIN)
+public class Resource {
+
+    @GET
+    public String getRes() {
+        return "Hello World";
+    }
+}
